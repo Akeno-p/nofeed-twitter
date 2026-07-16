3cc8efe5e37ae62ad7d777adb02c4ebe4c6f79b8
Akeno-p <camifoure@gmail.com>
2026-07-17 07:44:51 +0900

    chore(admin): regiser all models to admin site

--- dm/admin.py
+++ dm/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Conversation, DirectMessage, DirectMessageMedia
 
-# Register your models here.
+admin.site.register(Conversation)
+admin.site.register(DirectMessage)
+admin.site.register(DirectMessageMedia)
