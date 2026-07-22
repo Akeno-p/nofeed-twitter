b93ee91c673531ad5c51a0864fa80fcbcc67f3a1
Akeno-p <camifoure@gmail.com>
2026-07-23 05:53:30 +0900

    feat(users): add TOTP setup page with QR code and verification

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("do_login/", views.do_login, name="do_login"),
     path("two_factor_auth/", views.two_factor_auth_view, name="two_factor_auth"),
+    path("two_factor_qrcode/", views.two_factor_qrcode_view, name="two_factor_qrcode"),
+    path(
+        "verify_two_factor_code/",
+        views.verify_two_factor_code,
+        name="verify_two_factor_code",
+    ),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate
+from .models import Account
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.http import JsonResponse
 from django.urls import reverse
+import pyotp
+import qrcode
+import io
+import base64
 
 
 def login_view(request):
@@ -45,9 +50,20 @@ def do_login(request):
     user = authenticate(request, username=username, password=password)
 
     if user:
-        return JsonResponse(
-            {"status": "success", "redirect_url": reverse("two_factor_auth")}
-        )
+        request.session["pending_user_id"] = user.id
+
+        if user.totp_secret:
+            return JsonResponse(
+                {"status": "success", "redirect_url": reverse("two_factor_auth")}
+            )
+        else:
+
+            return JsonResponse(
+                {
+                    "status": "success",
+                    "redirect_url": reverse("two_factor_qrcode"),
+                }
+            )
 
     return JsonResponse(
         {
@@ -60,3 +76,76 @@ def do_login(request):
 
 def two_factor_auth_view(request):
     return render(request, "users/two_factor_auth.html")
+
+
+def two_factor_qrcode_view(request):
+    user_id = request.session.get("pending_user_id")
+
+    totp_secret = (
+        Account.objects.filter(id=user_id).values_list("totp_secret", flat=True).first()
+    )
+
+    # パスワードとユーザー名が流出した場合、ログイン後[users/verify_two_factor_code/]に
+    # 直接アクセスすることで、秘密鍵を再設定できてしまうのを防ぐため
+    if totp_secret:
+        return redirect("login")
+
+    qrcode_b64 = _setup_totp_secret(request)
+    return render(
+        request,
+        "users/two_factor_qrcode.html",
+        {"qrcode": qrcode_b64},
+    )
+
+
+def _setup_totp_secret(request):
+    """秘密鍵の生成とQRコードの生成
+
+    戻り値はQRコードのbase64
+    """
+    user_id = request.session.get("pending_user_id")
+    user_name = Account.objects.get(id=user_id).username
+
+    # すでにqrコード読み取り済みで[two_factor_qrcode.html]ページをリロードしてしまった場合、
+    # 秘密鍵が一致しなくなるため
+    if not request.session.get("interim_totp_secret"):
+        interim_totp_secret = pyotp.random_base32()
+    else:
+        interim_totp_secret = request.session.get("interim_totp_secret")
+
+    request.session["interim_totp_secret"] = interim_totp_secret
+
+    url = pyotp.TOTP(interim_totp_secret).provisioning_uri(
+        name=user_name, issuer_name="nofeed-twitter"
+    )
+
+    qrcode_img = qrcode.make(url)
+
+    buffer = io.BytesIO()
+
+    qrcode_img.save(buffer)
+    qrcode_b64 = base64.b64encode(buffer.getvalue()).decode()
+
+    return qrcode_b64
+
+
+def verify_two_factor_code(request):
+    """入力された認証キーが正しいか確認
+
+    正しい場合はAccount.totp_secretに保存する。
+    """
+    two_factor_code = request.POST.get("twoFactorCode")
+    interim_totp_secret = request.session.get("interim_totp_secret")
+
+    totp = pyotp.TOTP(interim_totp_secret)
+
+    if totp.verify(two_factor_code):
+        user = Account.objects.get(id=request.session.get("pending_user_id"))
+        user.totp_secret = interim_totp_secret
+        user.save(update_fields=["totp_secret"])
+
+        login(request, user)
+
+        return JsonResponse({"status": "success", "redirect_url": reverse("tweets")})
+    else:
+        return JsonResponse({"status": "fail", "message": "認証コードが一致しません。"})
