afcc7f71a43fab6c609dc534c43984c6ea1fc018
Akeno-p <camifoure@gmail.com>
2026-08-02 16:31:58 +0900

    feat(home): display my user info in the home sidebar

--- api_usage/views.py
+++ api_usage/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
+@login_required
 def api_usage_view(request):
     return render(request, "api_usage/api_usage.html")
--- tweets/views.py
+++ tweets/views.py
@@ -1,8 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
+@login_required
 def tweets_view(request):
-    return render(request, "tweets/tweets.html")
+    return render(
+        request,
+        "tweets/tweets.html",
+    )
 
+@login_required
 def reply_view(request):
-    return render(request, "tweets/reply.html")
+    return render(
+        request,
+        "tweets/reply.html",
+    )
