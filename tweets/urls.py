0ca393da572ec32ea32aac82a22d0a2afc09df52
Akeno-p <camifoure@gmail.com>
2026-08-04 06:23:14 +0900

    feat(tweet): add tweet posting

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path("", views.tweets_view, name="tweets"),
     path("reply", views.reply_view, name="reply"),
+    path("tweet", views.tweet, name="tweet"),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,10 @@
+import requests
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render
+
+from common.utils import _update_tokens
+from common.x_api import TWITTER_TWEET_ENDPOINT
 
 
 @login_required
@@ -9,9 +14,42 @@ def tweets_view(request):
         "tweets/tweets.html",
     )
 
+
 @login_required
 def reply_view(request):
     return render(
         request,
         "tweets/reply.html",
     )
+
+
+@login_required
+def tweet(request):
+    tweet_text = request.POST.get("tweetText")
+
+    tweet_response = requests.post(
+        TWITTER_TWEET_ENDPOINT,
+        headers={"Authorization": f"Bearer {request.user.access_token}"},
+        json={"text": tweet_text},
+    )
+
+    if tweet_response.status_code == 401:
+        is_update_tokens = _update_tokens(request)
+
+        if not is_update_tokens:
+            return JsonResponse(
+                {"status": "error", "message": "アクセストークンの更新に失敗しました。"}
+            )
+
+        tweet_response = requests.post(
+            TWITTER_TWEET_ENDPOINT,
+            headers={"Authorization": f"Bearer {request.user.access_token}"},
+            json={"text": tweet_text},
+        )
+
+    if tweet_response.status_code != 201:
+        return JsonResponse(
+            {"status": "error", "message": "想定外のエラーが発生しました。"}
+        )
+    
+    return JsonResponse({"status": "success"})
