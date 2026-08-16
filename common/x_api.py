2692264a34492d1f4d68654c79e84fe053569019
Akeno-p <camifoure@gmail.com>
2026-08-16 10:45:43 +0900

    feat(replies): implement fetching and saving replies

--- common/x_api.py
+++ common/x_api.py
@@ -26,11 +26,15 @@ TWITTER_TOKEN_ENDPOINT = "https://api.x.com/2/oauth2/token"
 
 # ===== APIエンドポイント =====
 
-# GET：自分のユーザー情報
+# GET：自分のユーザー情報の取得
 TWITTER_USERS_ME_ENDPOINT = "https://api.x.com/2/users/me"
+# GET：ユーザー情報の取得
+TWITTER_USERS_ENDPOINT = "https://api.x.com/2/users"
 # POST：ツイート投稿
 TWITTER_TWEET_ENDPOINT = "https://api.x.com/2/tweets"
 # GET：ツイート単体取得
 TWITTER_GET_TWEET_ENDPOINT = "https://api.x.com/2/tweets/{tweet_id}"
 # GET：ユーザーのツイート一覧取得
 TWITTER_USER_TWEETS_ENDPOINT = "https://api.x.com/2/users/{user_id}/tweets"
+# GET：ツイートを検索して取得(直近7日分のみ)
+TWITTER_SEARCH_RECENT_ENDPOINT = "https://api.x.com/2/tweets/search/recent"
--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,6 @@
 import requests
 from django.contrib.auth.decorators import login_required
+from django.db.models import Max
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.template.loader import render_to_string
@@ -8,10 +9,13 @@ from django.utils import timezone
 from common.utils import _request_with_token_refresh
 from common.x_api import (
     TWITTER_GET_TWEET_ENDPOINT,
+    TWITTER_SEARCH_RECENT_ENDPOINT,
     TWITTER_TWEET_ENDPOINT,
     TWITTER_USER_TWEETS_ENDPOINT,
+    TWITTER_USERS_ENDPOINT,
 )
 from tweets.models import Tweet
+from users.models import User
 
 
 @login_required
@@ -156,23 +160,25 @@ def save_all_tweets(request):
 
         return all_tweets_response
 
-    request_status, get_all_tweets_result = _request_with_token_refresh(
+    get_all_tweets_status, get_all_tweets_result = _request_with_token_refresh(
         request, get_all_tweets, 200
     )
 
-    if request_status == "error":
+    if get_all_tweets_status == "error":
         return JsonResponse(get_all_tweets_result)
 
     all_tweets_list = get_all_tweets_result.json().get("data")
 
-    saved_tweet_list = list(
+    saved_tweet_ids = set(
         Tweet.objects.filter(author=request.user.user_id).values_list("id", flat=True)
     )
+
+    tweets_list = []
 
     for tweet in all_tweets_list:
         tweet_id = int(tweet.get("id"))
 
-        if tweet_id in saved_tweet_list:
+        if tweet_id in saved_tweet_ids:
             continue
 
         in_reply_to_tweet_id = None
@@ -198,8 +204,9 @@ def save_all_tweets(request):
             in_reply_to_tweet_id=in_reply_to_tweet_id,
             in_quoted_to_tweet_id=in_quoted_to_tweet_id,
         )
-        tweet.save()
+        tweets_list.append(tweet)
 
+    Tweet.objects.bulk_create(tweets_list)
     return JsonResponse({"status": "success"})
 
 
@@ -255,4 +262,127 @@ def reply(request):
 
 
 def save_replies(request):
-    pass
+
+    my_tweet_ids = Tweet.objects.filter(
+        author=request.user.user_id, in_reply_to_tweet_id__isnull=True
+    ).values_list("id", flat=True)
+
+    last_reply_id = (
+        Tweet.objects.filter(conversation_id__in=my_tweet_ids)
+        .exclude(author=request.user.user_id)
+        .aggregate(last_id=Max("id"))["last_id"]
+    )
+
+    def get_replies():
+        replies_response = requests.get(
+            TWITTER_SEARCH_RECENT_ENDPOINT,
+            headers={"Authorization": f"Bearer {request.user.access_token}"},
+            params={
+                "query": f"to:{request.user.user.username} -is:retweet -from:{request.user.user.username}",
+                "max_results": 100,
+                "post.fields": "created_at,author_id,conversation_id,referenced_tweets",
+                "since_id": last_reply_id,
+            },
+        )
+
+        return replies_response
+
+    replies_status, replies_result = _request_with_token_refresh(
+        request, get_replies, 200
+    )
+
+    if replies_status == "error":
+        JsonResponse(replies_result)
+
+    replies_response_list = replies_result.json().get("data")
+
+    replies_list = []
+    not_saved_user_ids = []
+    all_tweet_ids = set(Tweet.objects.values_list("id", flat=True))
+    saved_user_ids = set(User.objects.values_list("id", flat=True))
+
+    for reply in replies_response_list:
+        author_id = int(reply.get("author_id"))
+
+        if author_id not in saved_user_ids:
+            not_saved_user_ids.append(author_id)
+
+        in_reply_to_tweet_id = None
+        in_quoted_to_tweet_id = None
+
+        referenced_tweet_list = reply.get("referenced_tweets")
+
+        if referenced_tweet_list is not None:
+            for referenced_tweet in referenced_tweet_list:
+                referenced_tweet_type = referenced_tweet.get("type")
+
+                if referenced_tweet_type == "quoted":
+                    in_quoted_to_tweet_id = referenced_tweet.get("id")
+                elif referenced_tweet_type == "replied_to":
+                    in_reply_to_tweet_id = referenced_tweet.get("id")
+
+        reply_id = int(reply.get("id"))
+        if reply_id not in all_tweet_ids:
+            reply = Tweet(
+                id=reply_id,
+                author_id=reply.get("author_id"),
+                text=reply.get("text"),
+                created_at=reply.get("created_at"),
+                conversation_id=reply.get("conversation_id"),
+                in_reply_to_tweet_id=in_reply_to_tweet_id,
+                in_quoted_to_tweet_id=in_quoted_to_tweet_id,
+            )
+        replies_list.append(reply)
+
+    if not_saved_user_ids:
+        save_users_status, save_users_result = _save_users(request, not_saved_user_ids)
+
+        if save_users_status == "error":
+            return JsonResponse(save_users_result)
+
+    print(replies_list)
+
+    Tweet.objects.bulk_create(replies_list)
+
+    return JsonResponse({"status": "success"})
+
+
+def _get_users(request, user_ids):
+    """取得したいuserのidをリストで渡すとuserの情報が取得される"""
+
+    def users_request():
+        users_response = requests.get(
+            TWITTER_USERS_ENDPOINT,
+            headers={"Authorization": f"Bearer {request.user.access_token}"},
+            params={
+                "ids": ",".join(str(user_id) for user_id in user_ids),
+                "user.fields": "profile_image_url",
+            },
+        )
+        return users_response
+
+    return _request_with_token_refresh(request, users_request, 200)
+
+
+def _save_users(request, user_ids):
+    """保存したいuserのidをリストで渡すとuserの情報が保存される"""
+    user_info_status, user_info_result = _get_users(request, user_ids)
+
+    if user_info_status == "error":
+        return user_info_result
+
+    not_saved_user_info_list = user_info_result.json().get("data")
+
+    not_saved_users = []
+    for user_info in not_saved_user_info_list:
+        user = User(
+            id=user_info.get("id"),
+            username=user_info.get("username"),
+            name=user_info.get("name"),
+            profile_image_url=user_info.get("profile_image_url"),
+        )
+        not_saved_users.append(user)
+
+    User.objects.bulk_create(not_saved_users)
+
+    return "success", None
